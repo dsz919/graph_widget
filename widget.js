3e68d645d66fbee30ec4cfb36dd6cebbb1028df5
david <david@ubuntu.(none)>
2013-01-11 10:41:00 +1100

    make the widget update it's innerHTML

--- widget.js
+++ widget.js
@@ -6,6 +6,8 @@ widget = (function(){
         init: function(args){
             _args = args;
             console.log('got init: ' + args);
+            var widget_id = _args
+            document.getElementById(widget_id).innerHTML = "HI! I am widget!" + widget_id;
         },
         render: function() {
             alert("I got these! " + _args[0]);
@@ -13,9 +15,5 @@ widget = (function(){
     };
 }());
 
-document.getElementById('testing').innerHTML = "HELLO!";
-
 widgets.push(widget);
-
-console.log("widgets: " + widgets);
 
