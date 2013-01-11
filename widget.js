3a6cc0fe384730a9df8db86db5d113b073918ea0
david <david@ubuntu.(none)>
2013-01-11 11:06:47 +1100

    adding a little graph image to the widget

--- widget.js
+++ widget.js
@@ -7,7 +7,14 @@ widget = (function(){
             _args = args;
             console.log('got init: ' + args);
             var widget_id = _args
-            document.getElementById(widget_id).innerHTML = "HI! I am widget!" + widget_id;
+            var widget = document.getElementById(widget_id);
+            widget.innerHTML = "HI! I a graph widget!" + widget_id;
+            var image = document.createElement('img');
+            image.style.margin = "10px";
+            image.src = "/graph_widget/graph.png";
+            widget.appendChild(image);
+
+            
         },
         render: function() {
             alert("I got these! " + _args[0]);
